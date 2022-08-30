38796590ef21622cc85b43529b612300aa7c05b1
adamilton25 <adamilton250899@gmail.com>
2022-08-29 21:03:24 -0300

    default

---
+++ app/controllers/v1/products_controller.rb
@@ -0,0 +1,43 @@
+module V1
+    class ProductsController < ApplicationController
+
+        before_action :set_product, only: %i[update show destroy]
+
+        def index
+            @products = Product.all.limit(1000)
+            render json: {products: @products}
+        end
+
+        def show 
+            render json: {product: @product}
+        end
+
+        def create
+            @product = Product.create(product_params) 
+            render json: {product: @product}
+        end
+
+        def update
+            @product.update(product_params)
+            render json: {product: @product}
+        end
+
+        def destroy
+            @product.destroy
+
+            render json: {success: "Product deleted with Success"}
+        end
+
+        private
+
+        def product_params
+            params.require(:product).permit(:name,:description, :value, :quantity)
+        end
+
+        def set_product
+            @product = Product.find(params[:product_id])
+
+            return render json: {error: 'product not found'}, status: 404 if @product.nil?
+        end
+    end 
+end
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  namespace :v1 do
+    get 'products', to: 'products#index'
+    post '/products', to: 'products#create'
+    get '/products/:product_id', to: 'products#show'
+    put '/products/:product_id', to: 'products#update'
+  end
+end
